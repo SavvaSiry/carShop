f3287f7aab4864bc4e71466cf6bedac031f3682b
Savva <savvasiry@gmail.com>
2022-07-22 01:33:27 +0300

    Make carpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CarpageComponent } from './carpage/carpage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CatalogComponent
+    CatalogComponent,
+    CarpageComponent
   ],
   imports: [
     BrowserModule,
